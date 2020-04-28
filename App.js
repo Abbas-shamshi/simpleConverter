82440484b3b0f564fb56e15c1c962893a74f97dd
Abbas Shamshi <abbasshamshi72@gmail.com>
2020-04-28 02:56:53 -0400

    Home screen created

--- App.js
+++ App.js
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
 import React from 'react';
 import {
@@ -17,12 +10,10 @@ import {
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import Converter from './converter';
 
 const App: () => React$Node = () => {
   return (
@@ -32,40 +23,14 @@ const App: () => React$Node = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
+          
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
           )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
+          
+          <Converter />
         </ScrollView>
       </SafeAreaView>
     </>
--- readme.md
+++ readme.md
@@ -1,16 +1,18 @@
-#   Start New React Native Project
+##   Start New React Native Project
     
     npx react-native init simple-converter
 
-#   Run Application
+###   Check connected devices
+    adb devices
+
+
+##   Run Application
   
     npm run android
     npx react-native start
 
     npx react-native run-android
 
-#   Run Application on different port
+##   Run Application on different port
     npx react-native run-android --port=8080
 
-#   Check connected devices
-    adb devices
